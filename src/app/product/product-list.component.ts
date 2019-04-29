672eee79f7b42bc24902d94a451da3efcb49b4df
Andrey Veshtard <andrey.veshtard@ctco.lv>
2019-04-29 13:15:18 +0300

    Populate table with data, toggle button, filter field with two-way binding

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -6,4 +6,34 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Carden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "http://www.clker.com/cliparts/C/4/b/w/s/7/wheelbarrow-md.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2016",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "http://www.clker.com/cliparts/a/9/e/2/1195421765134612385rejon_Hammer.svg.med.png"
+        }
+    ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
