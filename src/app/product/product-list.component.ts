61e58b1c19ee907ca5f105b50cad644216b98de9
Andrey Veshtard <andrey.veshtard@ctco.lv>
2019-04-29 15:27:11 +0300

    IProduct interface, filter, custom pipe

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [
+
+    private _listFilter: string = 'cart';
+    public get listFilter(): string {
+        return this._listFilter;
+    }
+    public set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
-            "productId": 2,
-            "productName": "Carden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://www.clker.com/cliparts/C/4/b/w/s/7/wheelbarrow-md.png"
+            productId: 2,
+            productName: "Carden Cart",
+            productCode: "GDN-0023",
+            releaseDate: "March 18, 2016",
+            description: "15 gallon capacity rolling garden cart",
+            price: 32.99,
+            starRating: 4.2,
+            imageUrl: "http://www.clker.com/cliparts/C/4/b/w/s/7/wheelbarrow-md.png"
         },
         {
             "productId": 5,
@@ -33,7 +45,22 @@ export class ProductListComponent {
         }
     ];
 
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart'; 
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log("In OnInit");
     }
 }
