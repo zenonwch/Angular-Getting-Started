6951be193842b0567c55372145cccd4267aca798
Andrey Veshtard <andrey.veshtard@ctco.lv>
2019-04-29 10:43:49 +0300

    Some first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  pageTitle: string = 'Acme Product Management';
 }
